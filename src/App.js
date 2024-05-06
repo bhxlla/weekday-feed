6f174597d7b4e918979ff2a8f68f48f0bf1bc89a
varunbhalla19 <varunbhalla19@gmail.com>
2024-05-06 19:10:51 +0530

    implementation for infinite scroll list.

--- src/App.js
+++ src/App.js
@@ -15,38 +15,62 @@ function App() {
 const FeedPage = () => {
 
   const PAGE_SIZE = 20;
+  
   const [jobsList, setJobsList] = useState([]);
   const [offset, setOffset] = useState(0);
+  const [bottomLoading, setBottomLoading] = useState(false);
 
   const getData = useCallback(() => {
     const { fetchData, cancel } = Api.fetcher(offset, PAGE_SIZE)
+    setBottomLoading(true);
     fetchData()
-    .then(({ offset, jdList }) => {
-      setJobsList(list => [...list, ...jdList]);
-      setOffset(offset);
-    })
-    .catch(() => {})
+      .then(({ offset, jdList }) => {
+        setJobsList(list => [...list, ...jdList]);
+        setOffset(offset);
+      })
+      .catch(() => { })
+      .finally(() => setBottomLoading(false))
     return cancel;
-  }, [offset])
+  }, [offset, setBottomLoading])
 
   useEffect(() => {
     const cancelReq = getData()
     return cancelReq;
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  const onEndReached = useCallback(() => {
+    if(bottomLoading) { return; }
+    setBottomLoading(true);
+    getData();
+  }, [getData, bottomLoading, setBottomLoading])
 
   return (
     <section>
-      <Feed list={jobsList} />
+      <Feed 
+        list={jobsList} 
+        onEndReached={onEndReached}
+        />
     </section>
   )
 
 }
 
-const Feed = ({ list }) => {
+const Feed = ({ list, onEndReached }) => {
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.innerHeight + window.scrollY >= window.document.body.offsetHeight - 32) {
+        onEndReached();
+      }
+    }
+    window.addEventListener('scroll', onScroll)
+
+    return () => window.removeEventListener('scroll', onScroll);
+  }, [onEndReached])
 
   return list.map(el => (
-    <div key={el.jdUid} >
+    <div style={{ height: 100 }} key={el.jdUid} >
       <p>{el.companyName}, {el.jobRole}</p>
     </div>
   ))
