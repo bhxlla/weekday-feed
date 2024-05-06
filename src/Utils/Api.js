8a9db9d4a406ed62862a303c4a5c852f0fd56df2
varunbhalla19 <varunbhalla19@gmail.com>
2024-05-06 18:07:03 +0530

    added cancellation method to api util.

--- src/Utils/Api.js
+++ src/Utils/Api.js
@@ -1,19 +1,26 @@
-
 const ApiUtils = {
 
-    fetcher: (offset = 0, limit = 10) => fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", {
-        method: "POST",
-        body: JSON.stringify({
-            limit, offset
-        }),
-        headers: [
-            ["Content-Type", "application/json"]
-        ],
-    })
-        .then(res => res.json())
-        .then(res => ({ ...res, offset: offset + res.jdList.length, end: res.jdList.length < limit })),
+    fetcher: (offset = 0, limit = 10) => {
 
+        const controller = new AbortController();
+
+        return {
+            fetchData: () => fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", {
+                method: "POST",
+                body: JSON.stringify({
+                    limit, offset
+                }),
+                headers: [
+                    ["Content-Type", "application/json"]
+                ],
+                signal: controller.signal
+            })
+                .then(res => res.json())
+                .then(res => ({ ...res, offset: offset + res.jdList.length, end: res.jdList.length < limit })),
+
+            cancel: () => controller.abort('Cancelling')
+        }
+    }
 }
-
 
 export default ApiUtils;
